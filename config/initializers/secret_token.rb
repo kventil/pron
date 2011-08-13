ac50a60812857fb7a54bab221c228797fecae3cc
Robert <robert.bahmann@gmail.com>
2011-08-13 12:35:43 +0200

    meh.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pron::Application.config.secret_token = 'be58dca0b6584d8ee2c604ea408fa07450f22ac6af681ad6f21d84e43e74f72ef5bdb523e7086cd68f9f5c9fffebf7bc474877b28cf55b8db3304906796fd58e'
